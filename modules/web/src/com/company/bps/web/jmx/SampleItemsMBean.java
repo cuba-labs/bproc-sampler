458c1b368f27bf43844704fefa6401f29437311e
Maxim Gorbunkov <gorbunkov@haulmont.com>
2019-08-23 12:49:52 +0400

    Rename project from bproc-samples to bproc-sampler

--- modules/web/src/com/company/bps/web/jmx/SampleItemsMBean.java
+++ modules/web/src/com/company/bps/web/jmx/SampleItemsMBean.java
@@ -2,7 +2,7 @@ package com.company.bps.web.jmx;
 
 import com.haulmont.cuba.core.sys.jmx.JmxBean;
 
-@JmxBean(module = "bproc-samples", alias = "SampleItemsMBean")
+@JmxBean(module = "bproc-sampler", alias = "SampleItemsMBean")
 public interface SampleItemsMBean {
     void reset();
 }
